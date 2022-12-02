1e0902e0c490e78108fca30caf9b6690f7573811
Zain@Zain-ul-Abidin.com <itszain20@gmail.com>
2022-12-02 15:02:37 +0500

    check route implemented

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const tourRouter = require("./Routes/tourRoutes")
 const userRouter = require("./Routes/userRoutes")
 
 // Models
+const Tour = require("./Models/tourModel");
 
-// *************** Printing the environment **********************
+// ************ Printing the environment *******************
 // console.log("--> env : ", process.env.NODE_ENV)
 
 // Initializing express app
@@ -22,18 +23,16 @@ if (process.env.NODE_ENV === "development") {
     // Using installed middleware morgan
     app.use(morgan("dev"));
 }
-
 // Connecting with the database
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+
 }).then(() => {
     console.log("Connected to the database");
 }).catch((err) => {
     console.log("Error : ", err)
 })
-
-
 
 // 1). Middlewares
 app.use(express.json());
@@ -45,20 +44,41 @@ app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
     next();
 });
-
-
-app.use("/insert", (req, res) => {
-    console.log("Request body : ", req.body)
-    res.send("Hello")
-})
-
 // Manipulating the request using middle-ware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
+app.use("/insert", (req, res) => {
+    console.log("Request body : ", req.body)
 
+    const newTour = new Tour(req.body);
+    newTour.save((err) => {
+        if (err) {
+            res.status(404).json({
+                status: "Fail",
+                message: "Error adding new tour",
+                body: {
+                    err
+                }
+            })
+        }
+        else {
+            res.status(200).json({
+                status: "Success",
+                message: "New tour added to database",
+                body: {
+                    data: req.body
+                }
+            })
+        }
+    });
+
+})
+
+
+// Main routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
